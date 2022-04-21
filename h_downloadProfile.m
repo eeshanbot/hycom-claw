3538ece80b6f3b3ff5405438f64f4587702f60fc
eeshanbot <eeshan137@gmail.com>
2022-04-21 14:42:59 -0400

    works for 1994 to present day

---
+++ h_downloadProfile.m
@@ -0,0 +1,74 @@
+function [output] = h_downloadProfile(request,downloadURL)
+% h_downloadProfile downloads and sorts requested HYCOM file
+
+
+% create filename, delete local copy if it already exists
+filename = ['data-' datestr(request.time,30) '.csv'];
+if isfile(filename)
+    delete(filename);
+end
+
+% try to download file
+fileFound = false;
+try
+    websave(filename,downloadURL);
+    fileFound = true;
+catch
+    warning('not found');
+end
+
+if fileFound
+    
+     % check if file is empty
+    s = dir(filename);
+    if s.bytes == 0
+        warning('empty file: check URL!')
+        delete(filename);
+    end
+    
+    % read data into table
+    % discard variable names b/c HYCOM adds too many special characters
+    % Var 1 = time
+    % Var 2 = latitude[unit="degrees_north"]
+    % Var 3 = longitude[unit="degrees_east"]
+    % Var 4 = vertCoord[unit="m"]
+    % Var 5 = salinity[unit="psu"]
+    % Var 6 = water_temp[unit="degC"]
+    T = readtable(filename,'ReadVariableNames',false);
+    
+    % check if time matches
+    hycomTimeStr = T.Var1{1};
+    str = split(hycomTimeStr,'T');
+    str_ymd = split(str{1},'-');
+    str_hhmmss = split(str{2}(1:end-1),':');
+    hycomTime = [cellfun(@str2num,str_ymd); cellfun(@str2num,str_hhmmss)].';
+    timeDiff = 24.*abs(datenum(request.time - hycomTime));
+    
+    % output warning for non-nearest time neighbor i.e. missing HYCOM file
+    if timeDiff >= 1.5
+        warning('nearest neighbor time was missing! time difference = %1.2f hours',timeDiff);
+    end
+    
+    % process data
+    scale = 0.001;
+    offset = 20;
+    
+    T.Var5(T.Var5==-30000) = NaN;
+    T.Var5 = T.Var5 .* scale + offset;
+    
+    T.Var6(T.Var6==-30000) = NaN;
+    T.Var6 = T.Var6 .* scale + offset;
+    
+    % format into output structure
+    output.time = hycomTime;
+    output.requestTime = request.time;
+    
+    output.lat = T.Var2(1);
+    output.lon = T.Var3(1);
+    
+    output.depth = T.Var4;
+    output.sal = T.Var5;
+    output.temp = T.Var6;
+    output.ssp = snd_spd(output.depth,output.temp,output.sal);
+end
+
--- main_test_hycom_claw.m
+++ main_test_hycom_claw.m
@@ -7,39 +7,44 @@
 clear; clc; close all;
 
 %% parameters for test download
-request.time = [2014 03 23 11 20 00];
+request.time = [2020 03 08 12 00 00];
 request.lat = 72.3642;
 request.lon = -150.4196;
 
 %% string creation
-filename = ['data-' datestr(request.time,30) '.csv'];
 downloadURL = h_outputDownloadString(request);
-clipboard('copy',downloadURL);
+% clipboard('copy',downloadURL);
 
-try 
-    websave(filename,downloadURL);
-    
-    % check if empty
-    s = dir(filename);
-    if s.bytes == 0
-        warning('empty file: check URL!')
-        delete(filename);
-    end
-    
-    % check if time matches
-    T = readtable(filename,'Range','A2:A2');
-    hycomTimeStr = T.Var1{1};
-    str = split(hycomTimeStr,'T');
-    str_ymd = split(str{1},'-');
-    str_hhmmss = split(str{2}(1:end-1),':');
-    hycomTime = [cellfun(@str2num,str_ymd); cellfun(@str2num,str_hhmmss)].';
-    timeDiff = 24.*abs(datenum(request.time - hycomTime));
-    
-    if timeDiff >= 1.5
-        warning('nearest neighbor time was missing! time difference = %1.2f hours',timeDiff);
-    end
-catch 
-    warning('not found \n');
-end
+%% download
+output = h_downloadProfile(request,downloadURL);
 
-%% get data
+%% plot
+
+figure('name','hycom-plot','renderer','painters','position',[4900 4900 1000 700]); clf;
+t = tiledlayout(1,3,'TileSpacing','compact');
+
+% temperature
+nexttile();
+plot(output.temp,output.depth,'linewidth',3);
+set(gca,'ydir','reverse');
+grid on
+ylabel('depth [m]');
+xlabel('temp [degC]');
+
+% salinity
+nexttile();
+plot(output.sal,output.depth,'linewidth',3);
+set(gca,'ydir','reverse');
+xlim([29 36]);
+grid on
+xlabel('sal [psu]');
+
+% sound speed
+nexttile();
+plot(output.ssp,output.depth,'linewidth',3);
+set(gca,'ydir','reverse');
+grid on
+xlabel('ssp [m/s]');
+
+titstr = sprintf('%0.4f N, %0.4f E, %s',output.lat,output.lon,datestr(output.time));
+sgtitle({'HYCOM profile',titstr},'fontsize',16)
